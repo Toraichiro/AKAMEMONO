837062de4c0db6d8f1e72d8261b6a644b3d9ac2f
太田虎一郎 <deveneco01@deconeco.com>
2016-12-28 09:51:57 +0900

    プロジェクト設定

---
+++ Assets/Scripts/GameMain/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject chipPrefab;
+    public GameObject chipBase;
+
+	// Use this for initialization
+	void Start () {
+		for(int x = 0; x < 9; x++)
+        {
+            for(int y = 0; y < 9; y++)
+            {
+                GameObject obj = Instantiate(chipPrefab) as GameObject;
+                obj.transform.SetParent(chipBase.transform);
+                obj.transform.localPosition = new Vector3(x * 82 -(82*4f), y * 82 - (82 * 4f));
+            }
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
